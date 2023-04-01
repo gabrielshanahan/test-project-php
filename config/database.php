608c7c2b661829b366cebc2e530adc44f2e0a298
Gabriel Shanahan <GabrielShanahan@gmail.com>
2023-04-01 15:21:05 +0200

    App setup using Docker Compose

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+
+$database = array(
+	'address' 	=> getenv("MYSQL_ADDRESS"),
+	'username'	=> getenv("MYSQL_USER"),
+	'password'	=> getenv("MYSQL_PASS"),
+	'database'	=> getenv("MYSQL_DB")
+);
